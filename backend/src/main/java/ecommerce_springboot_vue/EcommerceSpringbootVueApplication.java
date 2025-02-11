9357878a9ec5b16b94c98a5660f1242cf1e0fa38
Sebastián Agüero <seba.aguero.96@gmail.com>
2025-02-11 00:12:37 -0300

    feat: Refactor application to utilize environment variables from .env file and add dotenv-java dependency for management

--- backend/src/main/java/ecommerce_springboot_vue/EcommerceSpringbootVueApplication.java
+++ backend/src/main/java/ecommerce_springboot_vue/EcommerceSpringbootVueApplication.java
@@ -3,10 +3,13 @@ package ecommerce_springboot_vue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ecommerce_springboot_vue.config.EnvConfig;
+
 @SpringBootApplication
 public class EcommerceSpringbootVueApplication {
 
 	public static void main(String[] args) {
+		EnvConfig.load(); // Load environment variables
 		SpringApplication.run(EcommerceSpringbootVueApplication.class, args);
 	}
 
